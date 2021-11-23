63fe390f5112ff34d0deae08d1f3c380b1a6c63b
Alex Clark <aclark@aclark.net>
2021-11-23 08:50:22 -0500

    Update

--- defaults.mk
+++ defaults.mk
@@ -1,4 +1,13 @@
-install: defaults-clock defaults-finder defaults-jumpcut defaults-screencapture
+defaults: defaults-clock defaults-finder defaults-jumpcut defaults-screencapture
+
+defaults-clock:
+	# Show date on clock
+	defaults write com.apple.menuextra.clock DateFormat "EEE MMM d  h:mm a"
+
+defaults-finder:
+	# New Finder Window opens HOME
+	defaults write com.apple.finder NewWindowTarget "PfHm"
+	defaults write com.apple.finder NewWindowTargetPath "file:///${HOME}/"
 
 defaults-jumpcut:
 	# Jumpcut
@@ -6,15 +15,6 @@ defaults-jumpcut:
 	defaults write net.sf.Jumpcut rememberNum 99
 	defaults write net.sf.Jumpcut loadOnStartup 1
 	defaults write net.sf.Jumpcut SUCheckAtStartup 1
-
-defaults-finder:
-	# New Finder Window opens HOME
-	defaults write com.apple.finder NewWindowTarget "PfHm"
-	defaults write com.apple.finder NewWindowTargetPath "file:///${HOME}/"
-
-defaults-clock:
-	# Show date on clock
-	defaults write com.apple.menuextra.clock DateFormat "EEE MMM d  h:mm a"
 
 defaults-screencapture:
 	# Screenshots
