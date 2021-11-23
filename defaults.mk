55527b8f8e0a424e435a47a7dae846ba45808942
Alex Clark <aclark@aclark.net>
2021-11-23 08:48:29 -0500

    Remove dock defaults

--- defaults.mk
+++ defaults.mk
@@ -16,22 +16,6 @@ defaults-clock:
 	# Show date on clock
 	defaults write com.apple.menuextra.clock DateFormat "EEE MMM d  h:mm a"
 
-defaults-dock:
-	# Dock
-	defaults delete com.apple.dock
-	killall Dock
-	defaults write com.apple.dock show-recents -bool FALSE
-	dockutil --add "/Applications/Google Chrome.app"
-	dockutil --add "/System/Applications/Utilities/Terminal.app"
-	dockutil --add "/Applications/Microsoft Outlook.app"
-	dockutil --add "/Applications/Cisco/Cisco AnyConnect Secure Mobility Client.app"
-	dockutil --add "/Applications/MIDIrouter.app"
-	dockutil --remove 'Keynote'
-	dockutil --remove 'Numbers'
-	dockutil --remove 'Pages'
-	defaults write com.apple.systempreferences AttentionPrefBundleIDs 0
-	killall Dock
-
 defaults-screencapture:
 	# Screenshots
 	defaults write com.apple.screencapture location /Users/alexclark/pCloud\ Drive/Screenshots
