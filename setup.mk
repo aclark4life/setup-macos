f5f02257382c107dd6b0c395f3873a6880d4fa5d
Alex Clark <aclark@aclark.net>
2022-03-13 12:13:16 -0400

    Update

--- setup.mk
+++ setup.mk
@@ -2,7 +2,7 @@ defaults-write:
 	# defaults write com.apple.menuextra.clock DateFormat "EEE MMM d  h:mm a"
 	# defaults write com.apple.finder NewWindowTarget "PfHm"
 	# defaults write com.apple.finder NewWindowTargetPath "file:///${HOME}/"
-	# defaults write com.apple.screencapture location /Users/alexclark/pCloud\ Drive/Screenshots
+	defaults write com.apple.screencapture location /Users/alexclark/pCloud\ Drive/Screenshots
 	defaults write net.sf.Jumpcut displayNum 99
 	defaults write net.sf.Jumpcut rememberNum 99
 	defaults write net.sf.Jumpcut loadOnStartup 1
