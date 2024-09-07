84f7452d21af57ab2cee514da04c9eee6ed71059
Jeffrey Alex Clark <aclark@aclark.net>
2024-09-06 21:23:12 -0400

    Update setup-macos files

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,5 +1,5 @@
 git clone https://aclark4life@github.com/aclark4life/dotfiles.git Dotfiles
-cd ~/Dotfiles
+cd Dotfiles
 git remote remove origin
 git remote add origin git@github.com:aclark4life/dotfiles.git
 git push --set-upstream origin main
